d5b1b7c0d8c4cd5d76e0e23f006117f2d82308ad
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-12-30 23:26:53 -0300

    Criando tela de login

--- MyBarber/src/screens/SignIn/index.js
+++ MyBarber/src/screens/SignIn/index.js
@@ -3,16 +3,38 @@ import { Text } from "react-native";
 import { 
     Container,
     InputArea,
- } from './styles';
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold
+} from './styles';
+
+import SignInput from "../../components/SignInput"; 
 
 import BarberLogo from '../../assets/barber.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () => {
     return (
         <Container>
             <BarberLogo width="100%" height="160" />
 
+            <InputArea>
+                <SignInput IconSvg={EmailIcon} />
+                
+                <SignInput IconSvg={LockIcon} />
 
+                <CustomButton>
+                    <CustomButtonText>LOGIN</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+
+            <SignMessageButton>
+                <SignMessageButtonText>Ainda não poussi uma conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+            </SignMessageButton>
 
         </Container>
     )
